a273938040f3de06f66b092a77f9162b33b47fd8
Waheed <g00341970@gmit.ie>
2019-03-05 17:10:17 +0000

    variables

--- variables.py
+++ variables.py
@@ -1,1 +1,8 @@
-print ("Hello Youtubers")
+a = 123.4
+b23 = 'Spam'
+first_name = "BILL"
+b = 432
+c = a + b
+print("a + b is " , c)
+print("first_name is " , first_name)
+print("Sorted Parts , After midnight or " , b23)
